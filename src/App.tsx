db0bd45ad7a7ba7ea78ef57743b30de615d06d0d
Ihar Voitau <igorgrime@gmail.com>
2024-05-24 22:48:05 +0300

    feat: solved filter mikrotask

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { FilterType, NewComponent } from "./components/NewComponent";
 
 function App() {
+  const [money] = useState([
+    { id: 1, banknots: "Dollars", value: 100, number: " a1234567890" },
+    { id: 2, banknots: "Dollars", value: 50, number: " z1234567890" },
+    { id: 3, banknots: "RUBLS", value: 100, number: " w1234567890" },
+    { id: 4, banknots: "Dollars", value: 100, number: " e1234567890" },
+    { id: 5, banknots: "Dollars", value: 50, number: " c1234567890" },
+    { id: 6, banknots: "RUBLS", value: 100, number: " r1234567890" },
+    { id: 7, banknots: "Dollars", value: 50, number: " x1234567890" },
+    { id: 8, banknots: "RUBLS", value: 50, number: " v1234567890" },
+  ]);
+
+  const [filter, setFilter] = useState("all");
+
+  let currentMoney = money;
+  if (filter === "RUBLS")
+    currentMoney = money.filter((item) => item.banknots === "RUBLS");
+  if (filter === "Dollars")
+    currentMoney = money.filter((item) => item.banknots === "Dollars");
+
+  const onClickFilterHandler = (nameBtn: FilterType) => {
+    setFilter(nameBtn);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NewComponent
+        currentMoney={currentMoney}
+        callback={onClickFilterHandler}
+      />
+    </>
   );
 }
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+type ButtonPropsType = {
+  name: string;
+  callBack: () => void;
+};
+
+export const Button = ({name, callBack}: ButtonPropsType) => {
+  const onClickHandler = () => callBack();
+
+  return <button onClick={onClickHandler}>{name}</button>;
+};
